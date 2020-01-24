8ed3cce514905523e4c4aaa91b59e08d012eb0bc
MxDkl <max@dekelnet.com>
2020-01-24 16:45:23 +0100

    Add files via upload

---
+++ innit.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+	apiKey: "AIzaSyB_6B-qnFaeXwiHETPElcYifZuJzfRD-Y4",
+	authDomain: "plol-585c5.firebaseapp.com",
+	databaseURL: "https://plol-585c5.firebaseio.com",
+	projectId: "plol-585c5",
+	storageBucket: "plol-585c5.appspot.com",
+	messagingSenderId: "559031622352",
+	appId: "1:559031622352:web:3532d965ca5024b6ac5cd5",
+	measurementId: "G-PJ55RF4MY9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
